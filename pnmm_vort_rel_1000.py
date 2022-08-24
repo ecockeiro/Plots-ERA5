8ab680afa6c13e7cc69766d8fef53b8e574ae994
Everson Coqueiro <91283739+ecockeiro@users.noreply.github.com>
2022-08-24 17:40:11 -0300

    Update pnmm_vort_rel_1000.py

--- pnmm_vort_rel_1000.py
+++ pnmm_vort_rel_1000.py
@@ -1,156 +1,180 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jun 16 10:00:35 2022
+Created on Wed Aug 24 17:06:39 2022
 
 @author: coqueiro
 """
 
-from netCDF4 import Dataset  # ler e escreve netcdf ## uma merda essa biblioteca usa xarray mesmo ##
-import xarray as xr # ler netCDF e muito mais <3
-import matplotlib.pyplot as plt  # biblioteca de plots
-import matplotlib.colors                  # Matplotlib colors  
-from datetime import datetime, timedelta  # basicas datas e tipos de tempo
-import cartopy, cartopy.crs as ccrs  # plot mapas
-import cartopy.feature as cfeature  # operacao de desenho em imagens
-import numpy as np  # importa o pacote numpy
-import cmocean 
-from cartopy.util import add_cyclic_point
-import mygrads as mg #pip install mygrads
-import metpy.calc as mpcalc
+#importando bibliotecas
+import cartopy.crs as ccrs
 import cartopy.feature as cfeature
+import matplotlib.pyplot as plt
+import metpy.calc as mpcalc
+from metpy.units import units
+import numpy as np
+import xarray as xr
 import cartopy.io.shapereader as shpreader # Import shapefiles
- 
-#------------------------------------------------------------------------------
-# abre os arquivos netCDF4
-file_1 = xr.open_dataset('/home/coqueiro/ufrj/micro/extra_normal_multi.nc', decode_times = False)
-file_2 = xr.open_dataset('/home/coqueiro/ufrj/micro/extra_normal_pnmm_fluxos.nc', decode_times = False)
+from datetime import datetime, timedelta  # basicas datas e tipos de tempo
+import cmocean
 
-# transformando as coordenadas de longitude dos arquivos
-file_1 = file_1.assign_coords(dict(longitude = (((file_1.longitude.values + 180) % 360) - 180))).sortby('longitude')
-file_2 = file_2.assign_coords(dict(longitude = (((file_2.longitude.values + 180) % 360) - 180))).sortby('longitude')
+#dataset
+file_0 = xr.open_dataset(
+    '/home/coqueiro/ufrj/fis_nuvens/dados/24_02_19_continental_multi.nc',
+    decode_times = False
+    )
 
+file_1 = xr.open_dataset(
+    '/home/coqueiro/ufrj/fis_nuvens/dados/24_02_19_continental_multi.nc'
+    ).metpy.parse_cf()
 
-# limites de lat e lon
-lat_min = -70.00
-lat_max = 10.00
-lon_min = -105.0
-lon_max = -20.00
+file_1 = file_1.assign_coords(dict(
+    longitude = (((file_1.longitude.values + 180) % 360) - 180))
+    ).sortby('longitude')
 
-# seleciona uma extensao minima para o recorte de dados
-extent = [lon_min, lon_max, lat_min, lat_max]
-
-# ler as lats e lons
-# velocidades
-lats = file_1.variables['latitude'][400:641]
-lons = file_1.variables['longitude'][400:641]
-
-#pressao
-lats = file_2.variables['latitude'][400:641]
-lons = file_2.variables['longitude'][400:641]
-
-# define a extensao da imagem
-img_extent = [lon_min, lon_max, lat_min, lat_max]
-
-# escolha o tamanho do plot em polegadas (largura x altura)
-plt.figure(figsize=(25,25))
-
-# usando a projeção da coordenada cilindrica equidistante 
-ax = plt.axes(projection=ccrs.PlateCarree())
-#ax.set_extent([extent[0], extent[2], extent[1], extent[3]], ccrs.PlateCarree())
-
-# adiciona continente e bordas
-shapefile = list(shpreader.Reader('/home/coqueiro/Downloads/br_unidades_da_federacao/BR_UF_2019.shp').geometries())
-ax.add_geometries(shapefile, ccrs.PlateCarree(), edgecolor='black', facecolor='none', linewidth=0.5)
-ax.coastlines(resolution='50m', color='black', linewidth=1.5)
-ax.add_feature(cartopy.feature.BORDERS, edgecolor='black', linewidth=1)
-gl = ax.gridlines(crs=ccrs.PlateCarree(), color='gray', alpha=1.0, linestyle='--', linewidth=0.25, xlocs=np.arange(-180, 180, 5), ylocs=np.arange(-90, 90, 5), draw_labels=True)
-gl.top_labels = False
-gl.right_labels = False
-
-#adiciona mascara de terra
-ax.add_feature(cfeature.LAND)
-
-#------------------------------------------------------------------------------
-# extrai a variavel
-# >>> como setar os valores corretamente no dado nc <<<
-# >>> ex: file_1.variables['var'][time[pos],level[pos],lat[pos_inicial]:lat[pos_final],lon[pos_inicial]:lon[pos_inicial]]
-# var = variavel que voce quer extrair dê um print(file_1.variables) para ver todas as variaveis
-# pos = posicao que tal latitude ocupa no dado .nc (180W = -180 = file_1['longitude'][0])
-# variação de 10 graus = 40 posições
-
-# np.array pra colocar a densidade linha senão dá error: AttributeError: 'Variable' object has no attribute 'ravel'
-
-i=24
-# pressao
-pressao = file_2.variables['msl'][i,400:641,400:641]/100
-
-# componente do geopotencial
-geopotencial = file_1.variables['z'][i,1,400:641,400:641]/10
-
-# 500 hPa
-vmin1 = 4950
-vmax1 = 5850
-
-data_min1 = vmin1
-data_max1 = vmax1
-interval1 = 25
-levels1 = np.arange(data_min1,data_max1,interval1)
+file_2 = xr.open_dataset(
+    '/home/coqueiro/ufrj/fis_nuvens/dados/24_02_19_continental_single.nc'
+    ).metpy.parse_cf()
 
 
-# componente do vento
-u_comp = np.array(file_1.variables['u'][i,5,400:641,400:641])
-v_comp = np.array(file_1.variables['v'][i,5,400:641,400:641])
-rel_vort = mg.hcurl(u_comp, v_comp, lats, lons)
-adv_rel_vort = mg.hadv(u_comp, v_comp, rel_vort, lats, lons)*10000000
-rel_vort = 100000*rel_vort
+file_2 = file_2.assign_coords(dict(
+    longitude = (((file_2.longitude.values + 180) % 360) - 180))
+    ).sortby('longitude')
+#
+#extent
+lon_slice = slice(-75., -20.)
+lat_slice = slice(10., -60.)
 
+#pega as lat/lon
+lats = file_1.latitude.sel(latitude=lat_slice).values
+lons = file_1.longitude.sel(longitude=lon_slice).values
 
-# adv vort relativa
-vmin2= -2
-vmax2= -0.10
-data_min2 = vmin2
-data_max2 = vmax2
-interval2 = 0.1
-levels2 = np.arange(data_min2,data_max2,interval2)
+#seta as variaveis
+level = 1000
 
-# pnmm
-vmin3 = 960
-vmax3 = 1028
-data_min3 = vmin3
-data_max3 = vmax3
-interval3 = 2
-levels3 = np.arange(data_min3,data_max3,interval3)
-
-# vort relativa
-vmin4= -22
-vmax4= -2
-data_min4 = vmin4
-data_max4 = vmax4
-interval4 = 2
-levels4 = np.arange(data_min4,data_max4,interval4)
-
-# plot vorticidade relativa 1000 hPa e pnmm
-img3 = plt.contourf(lons,lats, rel_vort, vmin=vmin4, vmax=vmax4, cmap=cmocean.cm.ice, levels = levels4, extend='both')
-img4 = ax.contour(lons, lats, pressao, colors='black', linewidths=0.8, levels=levels3)
-ax.clabel(img4, inline=1, inline_spacing=3, fontsize=14,fmt = '%3.0f', colors= 'black')
-
-
-# Getting the file time and date
-add_seconds = int(file_1.variables['time'][i])
-date = datetime(1900,1,1,0) + timedelta(hours=add_seconds)
-date_formatted = date.strftime('%Y-%m-%d %H')
-	
-# Add a title
-plt.title('Pnmm/vorticidade relativa (10⁻⁵.s⁻¹) - 1000 hPa', fontweight='bold', fontsize=35, loc='left')
-plt.title(f'{date_formatted}', fontsize=15, loc='right')
-
-# adiciona legenda 
-cb = plt.colorbar(img3, orientation = 'horizontal', pad=0.04, fraction=0.04)
-font_size = 20 # Adjust as appropriate.
-cb.ax.tick_params(labelsize=font_size)
-#----------------------------------------------------------------------------------------------------------- 
-# Salva imagem
-plt.savefig(f'pnmm_vort_1000 {date_formatted}.png', bbox_inches='tight')
+for i in range(len(file_1.variables['time'])):
+    
+    geopotencial = file_1.z.metpy.sel(
+        time = file_1.time[i] , vertical=level, latitude=lat_slice, longitude=lon_slice).metpy.unit_array.squeeze()
+    
+    u = file_1.u.metpy.sel(
+        time = file_1.time[i] , vertical=level, latitude=lat_slice, longitude=lon_slice).metpy.unit_array.squeeze()
+    
+    v = file_1.v.metpy.sel(
+        time = file_1.time[i] , vertical=level, latitude=lat_slice, longitude=lon_slice).metpy.unit_array.squeeze()
+    
+    pnmm = file_2.msl.metpy.sel(
+        time = file_1.time[i] , latitude=lat_slice, longitude=lon_slice).metpy.unit_array.squeeze()* 0.01 * units.hPa/units.Pa
     
     
+    dx, dy = mpcalc.lat_lon_grid_deltas(lons, lats)
+    
+    vorticidade = mpcalc.vorticity(u, v, dx=dx, dy=dy, x_dim=- 1, y_dim=- 2)*10**5
+    
+    # escolha o tamanho do plot em polegadas (largura x altura)
+    plt.figure(figsize=(25,25))
+    
+    # usando a projeção da coordenada cilindrica equidistante 
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    
+    
+    shapefile = list(
+        shpreader.Reader(
+        '/home/coqueiro/Downloads/br_unidades_da_federacao/BR_UF_2019.shp'
+        ).geometries()
+        )
+    
+    ax.add_geometries(
+        shapefile, ccrs.PlateCarree(), 
+        edgecolor = 'black', 
+        facecolor='none', 
+        linewidth=0.5
+        )
+    
+    # adiciona continente e bordas
+    ax.coastlines(resolution='50m', color='black', linewidth=1)
+    ax.add_feature(cfeature.BORDERS, edgecolor='black', linewidth=1)
+    gl = ax.gridlines(crs=ccrs.PlateCarree(),
+                      color='gray',
+                      alpha=1.0, 
+                      linestyle='--', 
+                      linewidth=0.5, 
+                      xlocs=np.arange(-180, 180, 5), 
+                      ylocs=np.arange(-90, 90, 5), 
+                      draw_labels=True
+                      )
+    gl.top_labels = False
+    gl.right_labels = False
+    
+    # intevalos da pnmm
+    intervalo_min2 = np.amin(np.array(pnmm))
+    intervalo_max2 = np.amax(np.array(pnmm))
+    interval_2 = 2              # de quanto em quanto voce quer que varie
+    levels_2 = np.arange(intervalo_min2, intervalo_max2, interval_2)
+    
+    # intevalos da divergencia - umidade
+    intervalo_min3 = -30
+    intervalo_max3 = 0
+    interval_3 = 2              # de quanto em quanto voce quer que varie
+    levels_3 = np.arange(intervalo_min3, intervalo_max3, interval_3)
+    
+    # adiciona mascara de terra
+    ax.add_feature(cfeature.LAND)
+    
+    # plota a imagem geopotencial
+    sombreado = ax.contourf(lons, 
+                            lats, 
+                            vorticidade, 
+                            cmap=cmocean.cm.ice, 
+                            levels = levels_3, 
+                            extend = 'both'
+                            )
+    
+    # plota a imagem pressao
+    contorno = ax.contour(lons,
+                          lats, 
+                          pnmm, 
+                          colors='black', 
+                          linewidths=0.8, 
+                          levels=levels_2
+                          )
+    
+    ax.clabel(contorno, 
+              inline = 1, 
+              inline_spacing = 1, 
+              fontsize=20, 
+              fmt = '%3.0f', 
+              colors= 'black'
+              )
+    
+    # adiciona legenda 
+    barra_de_cores = plt.colorbar(sombreado, 
+                                  orientation = 'horizontal', 
+                                  pad=0.04, 
+                                  fraction=0.04
+                                  )
+    font_size = 20 # Adjust as appropriate.
+    barra_de_cores.ax.tick_params(labelsize=font_size)
+    
+    # Getting the file time and date
+    add_seconds = int(file_0.variables['time'][i])
+    
+    date = datetime(1900,1,1,0) + timedelta(hours=add_seconds)
+    date_formatted = date.strftime('%Y-%m-%d %H')
+    	
+    # Add a title
+    plt.title('Vorticidade relativa (1/s) - '+ str(level) +' hPa',
+              fontweight='bold', 
+              fontsize=35, 
+              loc='left'
+              )
+    
+    plt.title(f'{date_formatted}', 
+              fontsize=20, 
+              loc='right'
+              )
+    
+    
+    #--------------------------------------------------------------------------
+    # Salva imagem
+    plt.savefig(f'vorticidade_relativa_{date_formatted}.png', bbox_inches='tight')
